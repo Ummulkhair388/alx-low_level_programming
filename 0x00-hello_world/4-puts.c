beb77380e6102a3ce2b1c248c38090e5f7035c4a
Ummulkhair388 <117747181+Ummulkhair388@users.noreply.github.com>
2022-12-10 11:51:34 +0000

    Fix code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-/*
- * Puts: Output simple string
-*/
-
 #include <stdio.h>
-
-int
-main(void)
+/**
+ * Description: main - Prints out a string to stout.
+ * Return: 0 if success
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
