ed6275b8eb1edcb55765fd50af35779305aedae7
Ummulkhair388 <117747181+Ummulkhair388@users.noreply.github.com>
2022-12-12 13:33:20 +0000

    Fixed errror with betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /*
-* Main - Check and print:-
+* main - Check and print:-
 * if the number is greater than 0: is positive
 * if the number is 0: is zero
 * if the number is less than 0: is negative
