2e3cb0ce4b4f2bbdfbeb3112b9521b646db0a694
Ummulkhair388 <117747181+Ummulkhair388@users.noreply.github.com>
2022-12-12 13:56:40 +0000

    Func/Print diffferent statement if condition is satisfied

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,25 +4,29 @@
 
 /**
  * main - assign a random number to the variable n each time it is executed
- * if the last digit of n is greater than 5: the string and is greater than 5
- * if the last digit of n is 0: the string and is 0
- * if the last digit of n is less than 6 and not 0: the \
- *          string and is less than 6 and not 0
+ * The string Last digit of n
  * Return: 0
  */
 
 int main(void)
 {
-    int n;
+int n;
+int lastn;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	int last_n = n % 10;
-    if (last_n > 5)
-        printf("Last digit of %d is %d and is greater than 5\n", n, last_n);
-    else if (last_n == 0)
-        printf("Last digit of %d is %d and is 0\n", n, last_n);
-    else if (last_n < 6 && last_n != 0)
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_n);
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastn = n % 10;
+if (lastn > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
+}
+else if (lastn == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, lastn);
+}
+else if (lastn < 6 && lastn != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
+}
+return (0);
 }
